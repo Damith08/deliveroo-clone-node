e40da5a2486b55f53e4a3e5b8fe1ec7f301429f8
Damith Samarasighe <108716149+DHS0004@users.noreply.github.com>
2023-09-14 10:21:55 +0530

    refactor: remove unwanted files

--- src/server.js
+++ src/server.js
@@ -1,38 +1,30 @@
-// const express = require("express");
-// const app = express();
-// const port = 3000;
+const express = require("express");
+const app = express();
+const port = 3000;
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`);
-// });
-// const http = require("http");
-// const app = require("./app");
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
 
-// const port = process.env.PORT || 3000;
+// const mongoose = require("mongoose");
+// const Restaurant = require("./models/restaurant.model.js");
 
-// app.set("port", port);
-// const server = http.createServer(app);
+// mongoose.connect(
+//   "mongodb+srv://damith:DHs123456@cluster0.yjyq82k.mongodb.net/?retryWrites=true&w=majority",
+// );
 
-// server.listen(port);
-const mongoose = require("mongoose");
-const Restaurant = require("./models/restaurant.model.js");
+// async function run() {
+//   const newRestaurant = await Restaurant.create({
+//     name: "abcd",
+//     address: "No5A,jasgdyu,jyasgdyug,Sri Lanka",
+//     email: "test@test.com",
+//     contact: 947751236541,
+//   });
+//   console.log(newRestaurant);
+// }
 
-mongoose.connect(
-  "mongodb+srv://damith:DHs123456@cluster0.yjyq82k.mongodb.net/?retryWrites=true&w=majority",
-);
-
-async function run() {
-  const newRestaurant = await Restaurant.create({
-    name: "abcd",
-    address: "No5A,jasgdyu,jyasgdyug,Sri Lanka",
-    email: "test@test.com",
-    contact: 947751236541,
-  });
-  console.log(newRestaurant);
-}
-
-run();
+// run();
