cd0e2d720020aec5b63f211ac0c755713152dc7c
Damith Samarasighe <108716149+DHS0004@users.noreply.github.com>
2023-09-21 09:06:02 +0530

    fix: remove id  object in the restaurant schema

--- src/models/restaurant.model.js
+++ src/models/restaurant.model.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const restaurantSchema = new Schema({
-  _id: { type: String, required: true, unique: true },
   name: { type: String, required: true },
   address: { type: String, required: true },
   email: { type: String, require: true, unique: true },
