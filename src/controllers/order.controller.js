1a93fb48c8d302adfd2106e9decd050182a83d1f
Damith Samarasighe <108716149+DHS0004@users.noreply.github.com>
2023-09-21 17:19:34 +0530

    feat: add services to the order controller

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -1,4 +1,7 @@
 const Order = require("../models/order.model");
+const dishDatabaseService = require("../services/dish.database.service");
+const restaurantDatabaseService = require("../services/restaurant.database.service");
+const userDatabaseService = require("../services/user.database.service");
 
 // get all orders
 exports.getAllOrders = (req, res) => {
@@ -44,27 +47,47 @@ exports.getOrder = (req, res) => {
 
 // create a new order
 exports.createOrder = (req, res) => {
-  const newOrder = new Order({
-    quantity: req.body.quantity,
-    placed_on: req.body.placed_on,
-    total_price: req.body.total_price,
-  });
+  dishDatabaseService
+    .findDishById(req.body.dish_id)
+    .then((foundDIsh) => {
+      restaurantDatabaseService
+        .findRestaurantById(req.body.restaurant_id)
+        .then((foundREstaurant) => {
+          userDatabaseService
+            .findUserById(req.body.user_id)
+            .then((foundUser) => {
+              const newOrder = new Order({
+                quantity: req.body.quantity,
+                timestamps: new Date(),
+                totalPrice: req.body.total_price,
+                dish: foundDIsh._id,
+                restaurant: foundREstaurant._id,
+                user: foundUser._id,
+              });
 
-  newOrder
-    .save()
-    .then((saveOrder) => {
-      return res.status(200).json({
-        success: "Create new order!!!",
-        data: saveOrder,
-      });
+              newOrder
+                .save()
+                .then((saveOrder) => {
+                  return res.status(200).json({
+                    success: "Create new order!!!",
+                    data: saveOrder,
+                  });
+                })
+                .catch((err) => {
+                  console.log(err, "createOrder");
+                  // TODO: handle type error from mongoose and return 400
+                  // TODO: handle required error from mongoose and return 400
+                  // TODO: handle unique error from mongoose and return 409
+                  return res.status(400).json({
+                    error: err,
+                  });
+                });
+            });
+        });
     })
     .catch((err) => {
-      console.log(err, "createOrder");
-      // TODO: handle type error from mongoose and return 400
-      // TODO: handle required error from mongoose and return 400
-      // TODO: handle unique error from mongoose and return 409
-      return res.status(400).json({
-        error: err,
+      return res.status(404).json({
+        message: "~DIsh CAnnot be found",
       });
     });
 };
