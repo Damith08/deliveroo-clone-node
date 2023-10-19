31c449f38952f6f92402abc5b9b3f12b1115317a
Damith Samarasighe <108716149+DHS0004@users.noreply.github.com>
2023-10-19 20:38:11 +0530

    feat: add env file to jwt authentication

--- src/services/jwt.service.js
+++ src/services/jwt.service.js
@@ -1,9 +1,11 @@
 const jwt = require("jsonwebtoken");
 
-console.log(process.env.TEST_KEY);
-
 exports.getToken = (user) => {
-  return jwt.sign({ email: user.email, sub: user._id }, process.env.TEST_KEY, {
-    expiresIn: "1h",
-  });
+  return jwt.sign(
+    { email: user.email, sub: user._id },
+    process.env.SECRET_KEY,
+    {
+      expiresIn: "1h",
+    },
+  );
 };
