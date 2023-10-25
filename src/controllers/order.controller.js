1f1b5aee7308b5a705bba62d0b5bb9d81715704a
Damith Samarasighe <108716149+DHS0004@users.noreply.github.com>
2023-10-25 22:24:29 +0530

    refactor: refactor promises to async await

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -4,189 +4,165 @@ const restaurantDatabaseService = require("../services/restaurant.database.servi
 const userDatabaseService = require("../services/user.database.service");
 
 // get all orders
-exports.getAllOrders = (req, res) => {
-  orderDatabaseService
-    .findAllOrders()
-    .then((allOrders) => {
-      return res.status(200).json({
-        success: true,
-        message: "Success",
-        data: allOrders,
-      });
-    })
-    .catch((err) => {
-      console.log(err, "getAllOrders");
-      // TODO: handle type error from mongoose and return 400
-      // TODO: handle required error from mongoose and return 400
-      // TODO: handle unique error from mongoose and return 409
-      return res.status(500).json({
-        success: false,
-        message: "Internal server error",
-        data: err,
-      });
+exports.getAllOrders = async (req, res) => {
+  try {
+    const allOrders = await orderDatabaseService.findAllOrders();
+
+    return res.status(200).json({
+      success: true,
+      message: "Success",
+      data: allOrders,
     });
+  } catch (err) {
+    // TODO: handle type error from mongoose and return 400
+    // TODO: handle required error from mongoose and return 400
+    // TODO: handle unique error from mongoose and return 409
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      data: err,
+    });
+  }
 };
 
 // get a single order
-exports.getOrder = (req, res) => {
-  const id = req.params.id;
-  orderDatabaseService
-    .findOrderById(id)
-    .then((order) => {
-      return res.status(200).json({
-        success: true,
-        message: "Success",
-        data: order,
-      });
-    })
-    .catch((err) => {
-      console.log(err, "find order");
-      // TODO: handle type error from mongoose and return 400
-      // TODO: handle required error from mongoose and return 400
-      // TODO: handle unique error from mongoose and return 409
-      return res.status(500).json({
-        success: false,
-        message: "Internal server error",
-        data: err,
-      });
+exports.getOrder = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const order = await orderDatabaseService.findOrderById(id);
+
+    return res.status(200).json({
+      success: true,
+      message: "Success",
+      data: order,
     });
+  } catch (err) {
+    console.log(err, "find order");
+    // TODO: handle type error from mongoose and return 400
+    // TODO: handle required error from mongoose and return 400
+    // TODO: handle unique error from mongoose and return 409
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      data: err,
+    });
+  }
 };
 
 // create a new order
-exports.createOrder = (req, res) => {
-  dishDatabaseService
-    .findDishById(req.body.dish_id)
-    .then((foundDish) => {
-      if (!foundDish) {
-        return res.status(404).json({
-          success: false,
-          message: "Dish not found",
-          data: err,
-        });
-      } else {
-        restaurantDatabaseService
-          .findRestaurantById(req.body.restaurant_id)
-          .then((foundRestaurant) => {
-            if (!foundRestaurant) {
-              return res.status(404).json({
-                success: false,
-                message: "Restaurant not found",
-                data: err,
-              });
-            } else {
-              userDatabaseService.findUserById(req.userId).then((foundUser) => {
-                if (!foundUser) {
-                  return res.status(404).json({
-                    success: false,
-                    message: "User not found",
-                    data: err,
-                  });
-                } else {
-                  orderDatabaseService
-                    .createNewOrder({
-                      quantity: req.body.quantity,
-                      totalPrice: req.body.total_price,
-                      dishId: foundDish._id,
-                      restaurantId: foundRestaurant._id,
-                      userId: foundUser._id,
-                    })
-                    .then((saveOrder) => {
-                      return res.status(201).json({
-                        success: true,
-                        message: "Success",
-                        data: saveOrder,
-                      });
-                    })
-                    .catch((err) => {
-                      console.log(err, "createOrder");
-                      // TODO: handle type error from mongoose and return 400
-                      // TODO: handle required error from mongoose and return 400
-                      // TODO: handle unique error from mongoose and return 409
-                      return res.status(500).json({
-                        success: false,
-                        message: "Internal server error",
-                        data: err,
-                      });
-                    });
-                }
-              });
-            }
-          })
-          .catch((err) => {
-            return res.status(500).json({
-              success: false,
-              message: "Internal server error",
-              data: err,
-            });
-          });
-      }
-    })
-    .catch((err) => {
-      return res.status(500).json({
+exports.createOrder = async (req, res) => {
+  try {
+    const foundDish = await dishDatabaseService.findDishById(req.body.dish_id);
+
+    if (!foundDish) {
+      return res.status(404).json({
         success: false,
-        message: "Internal server error",
+        message: "Dish not found",
         data: err,
       });
+    }
+
+    const foundRestaurant = await restaurantDatabaseService.findRestaurantById(
+      req.body.restaurant_id,
+    );
+
+    if (!foundRestaurant) {
+      return res.status(404).json({
+        success: false,
+        message: "Restaurant not found",
+        data: err,
+      });
+    }
+
+    const foundUser = await userDatabaseService.findUserById(req.userId);
+
+    if (!foundUser) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+        data: err,
+      });
+    }
+
+    const createOrder = await orderDatabaseService.createNewOrder({
+      quantity: req.body.quantity,
+      totalPrice: req.body.total_price,
+      dishId: foundDish._id,
+      restaurantId: foundRestaurant._id,
+      userId: foundUser._id,
     });
+
+    return res.status(201).json({
+      success: true,
+      message: "Success",
+      data: createOrder,
+    });
+  } catch (err) {
+    console.log(err, "createOrder");
+    // TODO: handle type error from mongoose and return 400
+    // TODO: handle required error from mongoose and return 400
+    // TODO: handle unique error from mongoose and return 409
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      data: err,
+    });
+  }
 };
 
 // update order partially
-exports.updateOrderPartially = (req, res) => {
-  const id = req.params.id;
-  orderDatabaseService
-    .findOrderByIdAndUpdate(id, {
+exports.updateOrderPartially = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const orderUpdate = await orderDatabaseService.findOrderByIdAndUpdate(id, {
       quantity: req.body.quantity,
       totalPrice: req.body.total_price,
-    })
-    .then((orderUpdate) => {
-      return res.status(200).json({
-        success: true,
-        message: "Success",
-        data: orderUpdate,
-      });
-    })
-    .catch((err) => {
-      console.log(err, "updateOrder");
-      // TODO: handle type error from mongoose and return 400
-      // TODO: handle required error from mongoose and return 400
-      // TODO: handle unique error from mongoose and return 409
-      return res.status(500).json({
-        success: false,
-        message: "Internal server error",
-        data: err,
-      });
     });
-};
 
+    return res.status(200).json({
+      success: true,
+      message: "Success",
+      data: orderUpdate,
+    });
+  } catch (err) {
+    console.log(err, "updateOrder");
+    // TODO: handle type error from mongoose and return 400
+    // TODO: handle required error from mongoose and return 400
+    // TODO: handle unique error from mongoose and return 409
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      data: err,
+    });
+  }
+};
 // update order
-exports.updateOrder = (req, res) => {
-  const id = req.params.id;
-  orderDatabaseService
-    .findOrderByIdAndUpdate(id, {
+exports.updateOrder = async (req, res) => {
+  try {
+    const orderUpdate = await orderDatabaseService.findOrderByIdAndUpdate(id, {
       quantity: req.body.quantity,
       totalPrice: req.body.total_price,
       dish: req.body.dish_id,
       restaurant: req.body.restaurant_id,
       user: req.body.user_id,
-    })
-    .then((orderUpdate) => {
-      return res.status(200).json({
-        success: true,
-        message: "Success",
-        data: orderUpdate,
-      });
-    })
-    .catch((err) => {
-      console.log(err, "updateOrder");
-      // TODO: handle type error from mongoose and return 400
-      // TODO: handle required error from mongoose and return 400
-      // TODO: handle unique error from mongoose and return 409
-      return res.status(500).json({
-        success: false,
-        message: "Internal server error",
-        data: err,
-      });
     });
+
+    return res.status(200).json({
+      success: true,
+      message: "Success",
+      data: orderUpdate,
+    });
+  } catch (err) {
+    console.log(err, "updateOrder");
+    // TODO: handle type error from mongoose and return 400
+    // TODO: handle required error from mongoose and return 400
+    // TODO: handle unique error from mongoose and return 409
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      data: err,
+    });
+  }
 };
 
 // delete order
@@ -211,4 +187,25 @@ exports.deleteOrder = (req, res) => {
         data: err,
       });
     });
+};
+
+exports.deleteOrder = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const orderDelete = await orderDatabaseService.findOrderByIdAndDelete(id);
+    return res.status(200).json({
+      success: true,
+      message: "Success",
+      data: orderDelete,
+    });
+  } catch (err) {
+    console.log(err, "deleteOrder");
+    // TODO: handle required error from mongoose and return 400
+    // TODO: handle unique error from mongoose and return 409
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      data: err,
+    });
+  }
 };
