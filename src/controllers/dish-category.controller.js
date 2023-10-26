bcd297e1107d7ed5b524e551918d93d4ea981caa
Damith Samarasighe <108716149+DHS0004@users.noreply.github.com>
2023-10-26 11:03:55 +0530

    refactor: refactor promises to async await

--- src/controllers/dish-category.controller.js
+++ src/controllers/dish-category.controller.js
@@ -1,127 +1,122 @@
 const dishCategoryDatabaseService = require("../services/dish-categories.database.service");
 
 // get all dish categories
-exports.getAllDishCategories = (req, res) => {
-  dishCategoryDatabaseService
-    .findAllDishCategories()
-    .then((allDishCategories) => {
-      return res.status(200).json({
-        success: true,
-        message: "Success",
-        data: allDishCategories,
-      });
-    })
-    .catch((err) => {
-      console.log(err, "find allDishCategories");
-      // TODO: handle type error from mongoose and return 400
-      // TODO: handle required error from mongoose and return 400
-      // TODO: handle unique error from mongoose and return 409
-      return res.status(500).json({
-        success: false,
-        message: "Internal server error",
-        data: err,
-      });
+exports.getAllDishCategories = async (req, res) => {
+  try {
+    const allDishCategories =
+      await dishCategoryDatabaseService.findAllDishCategories();
+
+    return res.status(200).json({
+      success: true,
+      message: "Success",
+      data: allDishCategories,
     });
+  } catch (err) {
+    console.log(err, "find allDishCategories");
+    // TODO: handle type error from mongoose and return 400
+    // TODO: handle required error from mongoose and return 400
+    // TODO: handle unique error from mongoose and return 409
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      data: err,
+    });
+  }
 };
 
 // get a dish category
-exports.getDishCategory = (req, res) => {
-  const id = req.params.id;
-  dishCategoryDatabaseService
-    .findDishCategory(id)
-    .then((dishCategory) => {
-      return res.status(200).json({
-        success: true,
-        message: "Success",
-        data: dishCategory,
-      });
-    })
-    .catch((err) => {
-      console.log(err, "find allDishCategories");
-      // TODO: handle type error from mongoose and return 400
-      // TODO: handle required error from mongoose and return 400
-      // TODO: handle unique error from mongoose and return 409
-      return res.status(500).json({
-        success: false,
-        message: "Internal server error",
-        data: err,
-      });
+exports.getDishCategory = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const dishCategory = await dishCategoryDatabaseService.findDishCategory(id);
+    return res.status(200).json({
+      success: true,
+      message: "Success",
+      data: dishCategory,
     });
+  } catch (err) {
+    console.log(err, "find allDishCategories");
+    // TODO: handle type error from mongoose and return 400
+    // TODO: handle required error from mongoose and return 400
+    // TODO: handle unique error from mongoose and return 409
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      data: err,
+    });
+  }
 };
 
 // create dish category
-exports.createDishCategory = (req, res) => {
-  dishCategoryDatabaseService
-    .createNewDishCategory({
-      name: req.body.name,
-    })
-    .then((saveDishCategory) => {
-      return res.status(201).json({
-        success: true,
-        message: "Success",
-        data: saveDishCategory,
+exports.createDishCategory = async (req, res) => {
+  try {
+    const saveDishCategory =
+      await dishCategoryDatabaseService.createNewDishCategory({
+        name: req.body.name,
       });
-    })
-    .catch((err) => {
-      console.log(err, "createDishCategory");
-      // TODO: handle type error from mongoose and return 400
-      // TODO: handle required error from mongoose and return 400
-      // TODO: handle unique error from mongoose and return 409
-      return res.status(500).json({
-        success: false,
-        message: "Internal server error",
-        data: err,
-      });
+    return res.status(201).json({
+      success: true,
+      message: "Success",
+      data: saveDishCategory,
     });
+  } catch (err) {
+    console.log(err, "createDishCategory");
+    // TODO: handle type error from mongoose and return 400
+    // TODO: handle required error from mongoose and return 400
+    // TODO: handle unique error from mongoose and return 409
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      data: err,
+    });
+  }
 };
 
 // update dish category
-exports.updateDishCategory = (req, res) => {
-  const id = req.params.id;
-  dishCategoryDatabaseService
-    .findDishCategoryByIdAndUpdate(id, {
-      name: req.body.name,
-    })
-    .then((dishCategoryUpdate) => {
-      return res.status(200).json({
-        success: true,
-        message: "Success",
-        data: dishCategoryUpdate,
+exports.updateDishCategory = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const dishCategoryUpdate =
+      await dishCategoryDatabaseService.findDishCategoryByIdAndUpdate(id, {
+        name: req.body.name,
       });
-    })
-    .catch((err) => {
-      console.log(err, "updateDish");
-      // TODO: handle type error from mongoose and return 400
-      // TODO: handle required error from mongoose and return 400
-      // TODO: handle unique error from mongoose and return 409
-      return res.status(500).json({
-        success: false,
-        message: "Internal server error",
-        data: err,
-      });
+    return res.status(200).json({
+      success: true,
+      message: "Success",
+      data: dishCategoryUpdate,
     });
+  } catch (err) {
+    console.log(err, "updateDish");
+    // TODO: handle type error from mongoose and return 400
+    // TODO: handle required error from mongoose and return 400
+    // TODO: handle unique error from mongoose and return 409
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      data: err,
+    });
+  }
 };
 
 // delete dish category
-exports.deleteDishCategory = (req, res) => {
-  const id = req.params.id;
-  dishCategoryDatabaseService
-    .findDishCategoryByIdAndDelete(id)
-    .then((dishCategoryDelete) => {
-      return res.status(200).json({
-        success: true,
-        message: "Success",
-        data: dishCategoryDelete,
-      });
-    })
-    .catch((err) => {
-      console.log(err, "deleteDish");
-      // TODO: handle required error from mongoose and return 400
-      // TODO: handle unique error from mongoose and return 409
-      return res.status(500).json({
-        success: false,
-        message: "Internal server error",
-        data: err,
-      });
+exports.deleteDishCategory = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const dishCategoryDelete =
+      await dishCategoryDatabaseService.findDishCategoryByIdAndDelete(id);
+    return res.status(200).json({
+      success: true,
+      message: "Success",
+      data: dishCategoryDelete,
     });
+  } catch (err) {
+    console.log(err, "deleteDish");
+    // TODO: handle required error from mongoose and return 400
+    // TODO: handle unique error from mongoose and return 409
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      data: err,
+    });
+  }
 };
