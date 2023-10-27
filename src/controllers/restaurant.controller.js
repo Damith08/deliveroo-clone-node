309ec86cd6e6298100e936aab9d69691b46d65ae
Damith Samarasighe <108716149+DHS0004@users.noreply.github.com>
2023-10-27 15:12:44 +0530

    refactor: refactor promises to async await
    refactor promises in restaurant route to async await

--- src/controllers/restaurant.controller.js
+++ src/controllers/restaurant.controller.js
@@ -1,160 +1,154 @@
 const restaurantService = require("../services/restaurant.database.service");
 
 // create restaurant
-exports.createRestaurant = (req, res) => {
-  restaurantService
-    .createNewRestaurant({
+exports.createRestaurant = async (req, res) => {
+  try {
+    const savedRestaurant = await restaurantService.createNewRestaurant({
       name: req.body.name,
       address: req.body.address,
       email: req.body.email,
       contact: req.body.contact,
-    })
-    .then((savedRestaurant) => {
-      return res.status(201).json({
-        success: true,
-        message: "Success",
-        data: savedRestaurant,
-      });
-    })
-    .catch((err) => {
-      console.log(err, "createRestaurant");
-      // TODO: handle type error from mongoose and return 400
-      // TODO: handle required error from mongoose and return 400
-      // TODO: handle unique error from mongoose and return 409
-      return res.status(500).json({
-        success: false,
-        message: "Internal server error",
-        data: err,
-      });
     });
+    return res.status(201).json({
+      success: true,
+      message: "Success",
+      data: savedRestaurant,
+    });
+  } catch (err) {
+    console.log(err, "createRestaurant");
+    // TODO: handle type error from mongoose and return 400
+    // TODO: handle required error from mongoose and return 400
+    // TODO: handle unique error from mongoose and return 409
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      data: err,
+    });
+  }
 };
 
 // get all restaurants data
-exports.getAllRestaurants = (req, res) => {
-  restaurantService
-    .findAllRestaurants()
-    .then((foundRestaurants) => {
-      return res.status(200).json({
-        success: true,
-        message: "Success",
-        data: foundRestaurants,
-      });
-    })
-    .catch((err) => {
-      console.log(err, "find allRestaurants");
-      // TODO: handle type error from mongoose and return 400
-      // TODO: handle required error from mongoose and return 400
-      // TODO: handle unique error from mongoose and return 409
-      return res.status(500).json({
-        success: false,
-        message: "Internal server error",
-        data: err,
-      });
+exports.getAllRestaurants = async (req, res) => {
+  try {
+    const foundRestaurants = await restaurantService.findAllRestaurants();
+    return res.status(200).json({
+      success: true,
+      message: "Success",
+      data: foundRestaurants,
     });
+  } catch (err) {
+    console.log(err, "find allRestaurants");
+    // TODO: handle type error from mongoose and return 400
+    // TODO: handle required error from mongoose and return 400
+    // TODO: handle unique error from mongoose and return 409
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      data: err,
+    });
+  }
 };
 
-// get restaurant data
-exports.getRestaurant = (req, res) => {
-  const id = req.params.id;
-  restaurantService
-    .findRestaurantById(id)
-    .then((restaurant) => {
-      return res.status(200).json({
-        success: true,
-        message: "Success",
-        data: restaurant,
-      });
-    })
-    .catch((err) => {
-      console.log(err, "find restaurant");
-      // TODO: handle type error from mongoose and return 400
-      // TODO: handle required error from mongoose and return 400
-      // TODO: handle unique error from mongoose and return 409
-      return res.status(500).json({
-        success: false,
-        message: "Internal server error",
-        data: err,
-      });
+// get single restaurant data
+exports.getRestaurant = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const restaurant = await restaurantService.findRestaurantById(id);
+    return res.status(200).json({
+      success: true,
+      message: "Success",
+      data: restaurant,
     });
+  } catch (err) {
+    console.log(err, "find restaurant");
+    // TODO: handle type error from mongoose and return 400
+    // TODO: handle required error from mongoose and return 400
+    // TODO: handle unique error from mongoose and return 409
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      data: err,
+    });
+  }
 };
 
 // Update restaurant partially
-exports.updateRestaurantPartially = (req, res) => {
-  const id = req.params.id;
-  restaurantService
-    .findRestaurantByIdAndUpdate(id, {
-      address: req.body.address,
-      email: req.body.email,
-      contact: req.body.contact,
-    })
-    .then((updateRestaurantPartially) => {
-      return res.status(200).json({
-        success: true,
-        message: "Success",
-        data: updateRestaurantPartially,
+exports.updateRestaurantPartially = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updateRestaurantPartially =
+      await restaurantService.findRestaurantByIdAndUpdate(id, {
+        address: req.body.address,
+        email: req.body.email,
+        contact: req.body.contact,
       });
-    })
-    .catch((err) => {
-      console.log(err, "Cannot update");
-      // TODO: handle type error from mongoose and return 400
-      // TODO: handle required error from mongoose and return 400
-      // TODO: handle unique error from mongoose and return 409
-      return res.status(500).json({
-        success: false,
-        message: "Internal server error",
-        data: err,
-      });
+    return res.status(200).json({
+      success: true,
+      message: "Success",
+      data: updateRestaurantPartially,
     });
+  } catch (err) {
+    console.log(err, "Cannot update");
+    // TODO: handle type error from mongoose and return 400
+    // TODO: handle required error from mongoose and return 400
+    // TODO: handle unique error from mongoose and return 409
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      data: err,
+    });
+  }
 };
 
 // Update restaurant
-exports.updateRestaurant = (req, res) => {
-  const id = req.params.id;
-  restaurantService
-    .findRestaurantByIdAndUpdate(id, {
-      name: req.body.name,
-      address: req.body.address,
-      email: req.body.email,
-      contact: req.body.contact,
-    })
-    .then((updateRestaurant) => {
-      return res.status(200).json({
-        success: true,
-        message: "Success",
-        data: updateRestaurant,
+exports.updateRestaurant = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updateRestaurant =
+      await restaurantService.findRestaurantByIdAndUpdate(id, {
+        name: req.body.name,
+        address: req.body.address,
+        email: req.body.email,
+        contact: req.body.contact,
       });
-    })
-    .catch((err) => {
-      console.log(err, "Cannot update");
-      // TODO: handle type error from mongoose and return 400
-      // TODO: handle required error from mongoose and return 400
-      // TODO: handle unique error from mongoose and return 409
-      return res.status(500).json({
-        success: false,
-        message: "Internal server error",
-        data: err,
-      });
+    return res.status(200).json({
+      success: true,
+      message: "Success",
+      data: updateRestaurant,
     });
+  } catch (err) {
+    console.log(err, "Cannot update");
+    // TODO: handle type error from mongoose and return 400
+    // TODO: handle required error from mongoose and return 400
+    // TODO: handle unique error from mongoose and return 409
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      data: err,
+    });
+  }
 };
 
 // Delete Restaurant
-exports.deleteRestaurant = (req, res) => {
-  const id = req.params.id;
-  restaurantService
-    .findRestaurantByIdAndDelete(id)
-    .then((deleteRestaurant) => {
-      return res.status(200).json({
-        success: true,
-        message: "Success",
-        data: deleteRestaurant,
-      });
-    })
-    .catch((err) => {
-      console.log(err, "Cannot delete");
-      return res.status(500).json({
-        success: false,
-        message: "Internal server error",
-        data: err,
-      });
+exports.deleteRestaurant = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const deleteRestaurant =
+      await restaurantService.findRestaurantByIdAndDelete(id);
+    return res.status(200).json({
+      success: true,
+      message: "Success",
+      data: deleteRestaurant,
     });
+  } catch (err) {
+    console.log(err, "Cannot delete");
+    // TODO: handle type error from mongoose and return 400
+    // TODO: handle required error from mongoose and return 400
+    // TODO: handle unique error from mongoose and return 409
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      data: err,
+    });
+  }
 };
