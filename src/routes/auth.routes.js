07e65b66e3cfe90529097a6febeeda9f378307ba
Damith Samarasighe <108716149+DHS0004@users.noreply.github.com>
2023-11-01 12:07:48 +0530

    feat: add ajv validation to the login and register routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const authController = require("../controllers/auth.controller");
 const authRouter = express.Router();
-const userSchema = require("../schema/user.schema");
+const userRegisterSchema = require("../schema/user.schema");
+const userLoginSchema = require("../schema/user.login.schema");
 const formatMiddleware = require("../middleware/format.validation.middleware");
 // user login
-authRouter.post("/login", authController.loginUser);
+authRouter.post(
+  "/login",
+  formatMiddleware.schemaValidation(userLoginSchema.loginSchema),
+  authController.loginUser,
+);
 
 // user register
 authRouter.post(
   "/signup",
-  formatMiddleware.registerUserValidation(userSchema.registerSchema),
+  formatMiddleware.schemaValidation(userRegisterSchema.registerSchema),
   authController.signupUser,
 );
 
